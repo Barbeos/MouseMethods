63d7031c98b8a9881215f1be53df3c6566f4b548
Barbeos <32571022+Barbeos@users.noreply.github.com>
2017-10-30 21:53:41 +0100

    first commit

---
+++ TerrainMaker/Assets/OrbitOnMouseDrag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitOnMouseDrag : MonoBehaviour {
+	//public Object target;
+//	void Start(){
+//		target.material.color = Color.red;
+//	}
+
+	void LateUpdate(){
+//		if ( Input.GetMouseButtonDown (0)){ 
+//			RaycastHit hit; 
+//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+//			if ( Physics.Raycast (ray,out hit,100.0f)) {
+//				if (hit.transform.gameObject.GetComponent<Renderer> ().material.color == Color.red) {
+//					hit.transform.gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+//					Debug.Log("change to blue");
+//				}
+//				else if (hit.transform.gameObject.GetComponent<Renderer> ().material.color == Color.blue) {
+//					hit.transform.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+//					Debug.Log("change to red");
+//				}
+//				
+//				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+//			}
+//		}
+//		if (Input.GetMouseButton (0)) {
+//			RaycastHit hit; 
+//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//			if (Physics.Raycast (ray, out hit, 100.0f)) {
+//				Camera.main.transform.RotateAround (new Vector3 (hit.point.x, hit.point.y, hit.point.z),hit.point.x,3);
+//			}
+//		}
+
+
+
+			
+	}
+
+}
---
+++ TerrainMaker/Assets/Rotate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	Transform target;
+
+	//the target object
+	private float speedMod = 10.0f;
+	private Vector3 point;
+	Vector3 mousePos;
+
+	void Start ()
+	{	//Set up things on the start method
+		//point = target.position;//get target's coords
+		//transform.LookAt (point);//makes the camera look to it
+	}
+
+	void Update ()
+	{	//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit; 
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 100.0f)) {
+				point = hit.transform.position;
+			}
+		}
+			
+		if (Input.GetMouseButton (0)) {
+
+				transform.LookAt (point);//makes the camera look to it
+				transform.RotateAround (point, new Vector3 (0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * speedMod);
+			
+		}
+
+		if (Input.GetMouseButtonDown(1)) {
+			mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		}
+
+		if (Input.GetMouseButton (1)) {
+			Camera.main.transform.position = (Camera.main.ScreenToWorldPoint (Input.mousePosition) - mousePos);
+
+		}
+	}
+}
